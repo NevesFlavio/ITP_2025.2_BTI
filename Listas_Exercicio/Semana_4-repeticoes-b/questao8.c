1b41f85c7116b1386e47a1807b634ea4a751d9c1
Flávio Henrique de Oliveira Neves <flavio.neves999@hotmail.com>
2025-09-24 09:12:43 -0300

    Compra de Placas de Alumínio

---
+++ Listas_Exercicio/Semana_4-repeticoes-b/questao8.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main() {
+    int preco1, preco2, qtd;  
+
+    printf("Digite o preco do primeiro fornecedor: ");
+    scanf("%d", &preco1);
+
+    printf("Digite o preco do segundo fornecedor: ");
+    scanf("%d", &preco2);
+
+    printf("Digite a quantia disponivel: ");
+    scanf("%d", &qtd);
+
+    int minresto = qtd + 1;
+    int max_i = -1, max_j = -1;
+
+    for (int i = 0; i <= 10; i++) {
+        for (int j = 0; j <= 10; j++) {
+            int custo = i * preco1 + j * preco2;
+            if (custo<= qtd) {
+                int resto = qtd - custo;
+                printf("Comprando %d do primeiro e %d do segundo, resta %d\n", i, j, resto);
+                if (resto < minresto) {
+                    minresto = resto;
+                    max_i = i;
+                    max_j = j;
+                }
+            }
+        }
+    }
+
+    if (max_i != -1) {
+        printf("\nMenor restante = %d (comprando %d do 1o e %d do 2o).\n", minresto, max_i, max_j);
+    } else {
+        printf("Nenhuma compra possivel com o valor disponivel");
+    }
+
+    return 0;
+}

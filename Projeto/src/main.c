4b66b3b82bc4c602dc2ef870877fbd7d3bbc753f
Flávio Henrique de Oliveira Neves <flavio.neves999@hotmail.com>
2025-09-27 17:09:41 -0300

    Mapa com opção de dificuldade

--- Projeto/src/main.c
+++ Projeto/src/main.c
@@ -1,51 +1,79 @@
 #include <stdio.h>
 
-#define LIN 9                                           // Define o número de linhas e colunas como "iniciante"                         
-#define COL 9
 
-
-void inicializarJogo(char mapa[LIN][COL]) {
-    for (int i = 0; i < LIN; i++) {
-        for (int j = 0; j < COL; j++) {
+void inicializarJogo(int lin, int col, char mapa[lin][col]) {
+    for (int i = 1; i < lin; i++) {
+        for (int j = 1; j < col; j++) {
             mapa[i][j] = '_';  
         }
     }
 }
 
-
-void mostrarJogo(char mapa[LIN][COL]) {
+void mostrarJogo(int lin, int col, char mapa[lin][col]) {
     printf("   ");
-    for (int j = 0; j < COL; j++) {
-        printf("%d ", j);
+    for (int j = 1; j < col; j++) {
+        printf("%2d ", j);
     }
     printf("\n");
 
-    for (int i = 0; i < LIN; i++) {
-        printf("%d  ", i);
-        for (int j = 0; j < COL; j++) {
-            printf("%c ", mapa[i][j]);
+    for (int i = 1; i < lin; i++) {
+        printf("%2d  ", i);
+        for (int j = 1; j < col; j++) {
+            printf("%2c ", mapa[i][j]);
         }
         printf("\n");
     }
 }
 
-
-void colocarBombas(int bombas[LIN][COL]) {
-    for (int i = 0; i < LIN; i++) {
-        for (int j = 0; j < COL; j++) {
+void colocarBombas(int lin, int col, int bombas[lin][col]) {
+    for (int i = 1; i < lin; i++) {
+        for (int j = 1; j < col; j++) {
             bombas[i][j] = 0; 
         }
     }
 }
 
 int main() {
-    char mapa[LIN][COL];
-    int bombas[LIN][COL];
+    int lin, col;
+    int opcao;
 
-    inicializarJogo(mapa);
-    colocarBombas(bombas);
+do
+    {
+    printf("Escolha o nivel de dificuldade:\n");
+    printf("1 - Basico (9x9)\n");
+    printf("2 - Intermediario (16x16)\n");
+    printf("3 - Avancado (16x30)\n");
+    printf("Opcao: ");
+    scanf("%d", &opcao);
 
-    mostrarJogo(mapa);
+    
+        if (opcao == 1) {
+        lin = 9;
+        col = 9;
+    } else if (opcao == 2) {
+        lin = 16;
+        col = 16;
+    } else if (opcao == 3)
+    {
+        lin = 16;
+        col = 30;
+    } else {
+        printf("Opcao invalida. Escolha novamente:\n");
+       
+    }
+    
+    } while (opcao != 1 && opcao != 2 && opcao != 3);
+    
+    
+
+  
+    char mapa[lin][col];
+    int bombas[lin][col];
+
+    inicializarJogo(lin, col, mapa);
+    colocarBombas(lin, col, bombas);
+
+    mostrarJogo(lin, col, mapa);
 
     return 0;
 }
